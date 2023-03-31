687defd845e4049fb38ded0ed1db4220615a85f3
JesusUriel24 <solonosabes@gmail.com>
2023-03-31 01:40:34 -0600

    Empaquetando un proyecto

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const $app = document.getElementById("app");
-const API = "https://api.escuelajs.co/api/v1/products?offset=0&limit=10"
+const API = "https://api.escuelajs.co/api/v1/products?offset=5&limit=10";
 
 const main = async () => {
     const response = await fetch(API);
     const products = await response.json();
 
-const output = products?.map((products) => {
+const output = products?.map((product) => {
     return `
       <article class="Card">
-      <img src="${products.images}">
+      <img src="${product.images[0]}">
       <h2>
       ${product.title} <small>Precio $ ${product.price}</small>
       </h2>
